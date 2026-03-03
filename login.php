e4017c703b6e3588b30881f082f5660d30835aaf
Jun Yue <limjunyue66@gmail.com>
2026-03-03 10:13:42 +0800

    right version

--- login.php
+++ login.php
@@ -23,7 +23,7 @@ if ($result->num_rows > 0) {
     echo "Login successful!";
     // header("Location: dashboard.php");
 } else {
-    echo "Invalid password!";
+    echo "Invalid email!";
 }
 
 $conn->close();
